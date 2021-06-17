8bd8c2ccc112346f1eaeabe2a9a3a950a2c5bb24
cybercris <crssoares.js@gmail.com>
2021-06-17 16:13:59 -0300

    import poppins font in global style

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -1,19 +1,31 @@
+import {
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
+
 export default {
-    colors: {
-        primary: '#5636D3',
+  colors: {
+    primary: "#5636D3",
 
-        secondary: '#FF872C',
-        secondary_light: 'rgba(255, 135, 44, .3)',
+    secondary: "#FF872C",
+    secondary_light: "rgba(255, 135, 44, .3)",
 
-        success: '#12A454',
-        success_light: 'rgba(18, 164, 84, .5)',
+    success: "#12A454",
+    success_light: "rgba(18, 164, 84, .5)",
 
-        attention: '#E83F5B',
-        attention_light: 'rgba(232, 63, 91, .5)',
+    attention: "#E83F5B",
+    attention_light: "rgba(232, 63, 91, .5)",
 
-        shape: '#FFFFFF',
-        title: '#363F5F',
-        text: '#969CB2',
-        background: '#F0F2F5'
-    }
-}
+    shape: "#FFFFFF",
+    title: "#363F5F",
+    text: "#969CB2",
+    background: "#F0F2F5",
+  },
+
+  fonts: {
+    regular: 'Poppins_400Regular',
+    medium: 'Poppins_500Medium',
+    bold: 'Poppins_700Bold',
+  },
+};
